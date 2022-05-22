91d50593b9496ec1fae2cf43ad3f16b6c56c206a
rana135 <rana286090@gmail.com>
2022-05-22 22:28:06 +0600

    NotFound images added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Banner from './pages/Home/Banner/Banner';
+import NotFound from './pages/Shared/NotFound';
 
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/banner" element={<Banner/>} />
+
+
+        <Route path="*" element={<NotFound/>} />
       </Routes>
     </div>
   );
