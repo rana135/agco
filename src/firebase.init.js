d7a5817a0c37f69e101afd43a77808c29a55e255
rana135 <rana286090@gmail.com>
2023-03-01 22:45:34 +0600

    firebase.init change

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -4,14 +4,13 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId
+  apiKey: "AIzaSyCjdniZSQup0RNTycO_qDRYQ9JKclaHoV0",
+  authDomain: "agco-198f0.firebaseapp.com",
+  projectId: "agco-198f0",
+  storageBucket: "agco-198f0.appspot.com",
+  messagingSenderId: "411512755024",
+  appId: "1:411512755024:web:dbc8881d21cb74caab5255"
 };
 
 // Initialize Firebase
